605b3fc51d852671d685caed737a1bea88d95620
xiaohuixingjia <827729956@qq.com>
2018-07-06 16:29:06 +0800

    初始导入

---
+++ SpringCloudRibbonTest/src/test/java/com/study/ribbon/client/SpringCloudRibbonTest/SpringCloudRibbonTest/SpringCloudRibbonTest/SpringCloudRibbonTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.ribbon.client.SpringCloudRibbonTest.SpringCloudRibbonTest.SpringCloudRibbonTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudRibbonTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
