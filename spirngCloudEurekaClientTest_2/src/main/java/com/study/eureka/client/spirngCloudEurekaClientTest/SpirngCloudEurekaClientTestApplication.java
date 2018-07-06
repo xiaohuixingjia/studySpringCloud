33837cfa99a9ff3cfa90c9188db354e0b520b438
xiaohuixingjia <827729956@qq.com>
2018-07-06 13:45:47 +0800

    去掉多引用的jar包

--- spirngCloudEurekaClientTest_2/src/main/java/com/study/eureka/client/spirngCloudEurekaClientTest/SpirngCloudEurekaClientTestApplication.java
+++ spirngCloudEurekaClientTest_2/src/main/java/com/study/eureka/client/spirngCloudEurekaClientTest/SpirngCloudEurekaClientTestApplication.java
@@ -3,13 +3,10 @@ package com.study.eureka.client.spirngCloudEurekaClientTest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
