f7a49ae890842a690be61d4aecd7b06047689260
xiaohuixingjia <827729956@qq.com>
2018-07-09 18:37:22 +0800

    提交到github上

---
+++ SpringCloudZuulTest/src/main/java/com/study/zuul/SpringCloudZuulTest/SpringCloudZuulTest/SpringCloudZuulTestApplication.java
@@ -0,0 +1,12 @@
+package com.study.zuul.SpringCloudZuulTest.SpringCloudZuulTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulTestApplication.class, args);
+	}
+}
