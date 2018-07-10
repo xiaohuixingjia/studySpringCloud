22038c7228d993b911a0d2251fc54dd68244befa
xiaohuixingjia <827729956@qq.com>
2018-07-10 20:46:11 +0800

    a

---
+++ SpringCloudConfigClientTest/src/main/java/com/study/config/SpringCloudConfigClientTest/SpringCloudConfigClientTest/SpringCloudConfigClientTestApplication.java
@@ -0,0 +1,23 @@
+package com.study.config.SpringCloudConfigClientTest.SpringCloudConfigClientTest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCloudConfigClientTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientTestApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+}
---
+++ SpringCloudConfigClientTest/src/test/java/com/study/config/SpringCloudConfigClientTest/SpringCloudConfigClientTest/SpringCloudConfigClientTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.config.SpringCloudConfigClientTest.SpringCloudConfigClientTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigClientTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
